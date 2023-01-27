834577e8f32973c6a3bf8b39bcc2c216b2bc7613
Erik Andersen <etma@etma.dk>
2023-01-27 22:36:54 +0100

    fix mod download
    apply fix https://support.keenswh.com/spaceengineers/pc/topic/24978-1-201-013-dedicated-server-unable-to-download-some-mods

--- entrypoint-space_engineers.bash
+++ entrypoint-space_engineers.bash
@@ -2,4 +2,10 @@
 source ~/.profile
 source ~/.bash_profile
 cd /appdata/space-engineers/SpaceEngineersDedicated/DedicatedServer64/
+cp /appdata/space-engineers/SpaceEngineersDedicatedServer/steamclient.dll /appdata/space-engineers/SpaceEngineersDedicated/DedicatedServer64/
+cp /appdata/space-engineers/SpaceEngineersDedicatedServer/steamclient64.dll /appdata/space-engineers/SpaceEngineersDedicated/DedicatedServer64/
+cp /appdata/space-engineers/SpaceEngineersDedicatedServer/tier0_s.dll /appdata/space-engineers/SpaceEngineersDedicated/DedicatedServer64/
+cp /appdata/space-engineers/SpaceEngineersDedicatedServer/tier0_s64.dll /appdata/space-engineers/SpaceEngineersDedicated/DedicatedServer64/
+cp /appdata/space-engineers/SpaceEngineersDedicatedServer/vstdlib_s.dll /appdata/space-engineers/SpaceEngineersDedicated/DedicatedServer64/
+cp /appdata/space-engineers/SpaceEngineersDedicatedServer/vstdlib_s64.dll /appdata/space-engineers/SpaceEngineersDedicated/DedicatedServer64/
 env WINEARCH=win64 WINEDEBUG=-all WINEPREFIX=/wineprefix wine /appdata/space-engineers/SpaceEngineersDedicated/DedicatedServer64/SpaceEngineersDedicated.exe -noconsole -path Z:\\appdata\\space-engineers\\SpaceEngineersDedicated -ignorelastsession
